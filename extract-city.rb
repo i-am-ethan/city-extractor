5801c68cca303e12dd4152f4c346cddc41569894
ethan <izawaryosuke0101@gmail.com>
2024-09-29 17:22:16 +0900

    [Add]漢字のindexを返す

--- extract-city.rb
+++ extract-city.rb
@@ -53,13 +53,13 @@ def process_csv(csv_file_path)
   concatenated_addresses = []
 
   CSV.foreach(csv_file_path, headers: false, col_sep: ",", quote_char: "\"", encoding: "Shift_JIS:UTF-8", liberal_parsing: true) do |row|
-    kanji_address = concat_kanji_address(row)
-    romaji_address = concat_romaji_address(row)
-    concatenated_addresses << [kanji_address, romaji_address]
+    postal_code = format_postal_code(row[0])
+    kanji_address = replace_fullwidth_space(concat_kanji_address(row))
+    romaji_address = replace_fullwidth_space(concat_romaji_address(row))
+    concatenated_addresses << { postal_code: postal_code, kanji_address: kanji_address, romaji_address: romaji_address }
   end
 
   concatenated_addresses
-
 end
 
 def concat_kanji_address(row)
@@ -70,9 +70,30 @@ def concat_romaji_address(row)
   "#{row[4]} #{row[5]} #{row[6]}".strip
 end
 
+# 指定された郵便番号と市区町村名から漢字部分のインデックスを取得する
+def find_kanji_index_by_city_and_postal_code(processed_data, postal_code, city_name)
+  filtered_data = processed_data.select { |entry| entry[:postal_code] == postal_code }
+  filtered_data.each do |entry|
+    kanji_parts = entry[:kanji_address].split(" ")
+
+    # 指定された市区町村名がどの部分に含まれているかを検索
+    kanji_index = kanji_parts.find_index { |part| part.include?(city_name) }
+    if kanji_index
+      puts "*********************************************"
+      puts "Found '#{city_name}' in kanji_parts at index: #{kanji_index}"
+      puts "kanji_parts:'#{kanji_parts}'"
+      puts "*********************************************"
+      return kanji_index
+    end
+  end
+  puts "*********************************************"
+  puts "NotFound:kanji_index for #{city_name} with postal code #{postal_code}"
+  puts "*********************************************"
+  nil # 該当する市区町村名が見つからなかった場合
+end
+
 def main
   # (1)zipをダウンロードする
-  # url = "https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
   url = "https://www.post.japanpost.jp/zipcode/dl/roman/KEN_ALL_ROME.zip"
   download_dir = "./downloads"
   zip_filename = "ken_all.zip"
@@ -93,13 +114,20 @@ def main
   end 
 
   # ログ
-  puts "process_data$$$$$$$$$$$$$$$"
   p processed_data
-  puts "process_data$$$$$$$$$$$$$$$"
 
-  # (5)配列の中から指定した市区のローマ字を取得する
+  # (5)配列の中から指定した市区町村名の漢字インデックスを取得する
+  postal_code = "9010401" # 例として郵便番号を指定
+  city_name = "東風平" # 例として市区町村名を指定
+  kanji_index = find_kanji_index_by_city_and_postal_code(processed_data, postal_code, city_name)
 
-  # (5)成功したメッセージを表示する
+  if kanji_index
+    puts "The index of '#{city_name}' in kanji address is: #{kanji_index}"
+  else
+    puts "'#{city_name}' was not found in the processed data."
+  end
+
+  # (FINAL)成功したメッセージを表示する
   puts "process csv successly"
 end
 
